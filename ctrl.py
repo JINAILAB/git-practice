e781fc0a4a5f315d5c283efa5db0f712c29398f2
JINAILAB <parksonice@naver.com>
2023-09-24 09:31:45 +0900

    ssssss

--- ctrl.py
+++ ctrl.py
@@ -6,9 +6,18 @@ class Control:
     def calculate(self):
         pass
         
+    def lslslslsls(self):
+        pass
+    
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.calculate)
         self.view.btn2.clicked.connect(self.view.clearMessage)
+        
+    def connectSignals(self):
+        self.view.btn1.clicked.connect(self.calculate)
+        self.view.btn2.clicked.connect(self.lslslslsls)
+        
+    
     
     def sum(self, a, b): # 예외 처리 기능 추가
         try:
